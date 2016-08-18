5e7f370cb66048e5d6ecb41df165aab8771d2306
Gregivy <gpoi@mail.ru>
2016-08-19 00:19:22 +0300

    config.xml change

--- www/js/app.js
+++ www/js/app.js
@@ -136,7 +136,7 @@ angular.module('starter', ['ionic','ngCordova','angular-scroll-animate'])
   var tapEnabled = false; //enable tap take picture
   var dragEnabled = false; //enable preview box drag across the screen
   var toBack = true; //send preview box to the back of the webview
-  var rect = {x: 0, y: 44, width: screen.width * window.devicePixelRatio, height: (screen.height-44) * window.devicePixelRatio};
+  var rect = {x: 0, y: 44, width: 500, height: 500};
   cordova.plugins.camerapreview.startCamera(rect, "back", tapEnabled, dragEnabled, toBack);
 })
 
