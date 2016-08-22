e85e9779b0595d5c7264a530e95caa05251c7baa
Gregivy <gpoi@mail.ru>
2016-08-23 00:54:19 +0300

    new camera module

--- www/js/app.js
+++ www/js/app.js
@@ -186,7 +186,7 @@ angular.module('starter', ['ionic','ngCordova'])
     }
     CameraPreview.stopCamera();
   });
-  
+
   $scope.n = $stateParams["n"];
   $scope.item = $scope.items[$scope.n];
   $scope.title = $scope.item["name"][$scope.lang];
@@ -195,7 +195,8 @@ angular.module('starter', ['ionic','ngCordova'])
   var dragEnabled = false; //enable preview box drag across the screen
   var toBack = false; //send preview box to the back of the webview
   var rect = {x: 0, y: 44, width: 300, height: (500-44)};
-  CameraPreview.startCamera({x: 0, y: 44, width: screen.width, height: screen.height-44, camera: "back", tapPhoto: true, previewDrag: false, toBack: false});
+  alert(window.innerWidth+"x"+window.innerHeight);
+  CameraPreview.startCamera({x: 0, y: 44, width: window.innerWidth, height: window.innerHeight-44, camera: "back", tapPhoto: true, previewDrag: false, toBack: false});
   $scope.swapCamera = function () {
     CameraPreview.switchCamera();
   }
